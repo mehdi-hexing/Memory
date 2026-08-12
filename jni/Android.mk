1ba6c9c1c804f04020dffef87557deefa32a10ef
mehdi-hexing <127022233+mehdi-hexing@users.noreply.github.com>
2026-08-12 08:35:01 +0330

    Rename module and source file in Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := library
-LOCAL_SRC_FILES := library.c
+LOCAL_MODULE    := mylib
+LOCAL_SRC_FILES := mylib.c
 LOCAL_LDLIBS    := -llog
 
 include $(BUILD_SHARED_LIBRARY)
